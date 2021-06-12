3b95cbc5f77d7a974d5875036527533a2f87c87d
vmvansh27 <81684136+vmvansh27@users.noreply.github.com>
2021-06-12 16:01:14 +0530

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,6 @@
 '''
+Tic Tac Toe game
+Game Board:
    |   |  
 ---+---+---
    |   |
@@ -7,6 +9,7 @@
 '''
 dict={1:' ',2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' ',}
 
+# for checking win and draw conditions
 def checkwin():
     if(dict[1]==dict[2]==dict[3]!=' '): # Horizontal line
         return 1
@@ -25,19 +28,26 @@ def checkwin():
     elif(dict[3]==dict[5]==dict[7]!=' '): # Diagnal line
         return 1
     elif(dict[1]!=' ' and dict[2]!=' ' and dict[3]!=' ' and dict[4]!=' ' and dict[5]!=' ' and dict[6]!=' ' and dict[7]!=' ' and dict[8]!=' ' and dict[9]!=' '): # Draw
-        return -1
+        return -1 # Draw
     else:
-        return 0 # Continue
+        return 0 #  To Continue
 
-
+# for marking positions provided by the player 
 def marking(position,turn):
     if(turn=='x'):
         if dict[position]==' ': # For checking if that postion is occupied or not
             dict[position]='x'
+            return 1
+        else:
+            print("\nplace already occupied!! Please select empty box")
+            return -1
     else:
         if dict[position]==' ': # For checking if that postion is occupied or not
             dict[position]='o'
-
+            return 1
+        else:
+            print("place already occupied!! Please select empty box")
+            return -1
 
 # For printing the game board
 def printboard(board):
@@ -60,18 +70,19 @@ if __name__=='__main__':
     7 | 8 | 9
     ''')
     while(chkwin==0):
-        val=inc%2
+        val=inc%2 #logic for getting turns of both player alternatively
         if(val==1):
-            turn="x"
+            turn="x" 
             input_from_user=int(input("turn of player X: "))
-            marking(input_from_user,turn)
+            occupied=marking(input_from_user,turn)
         else:
             turn="o"
             input_from_user=int(input("Turn of player O: "))
-            marking(input_from_user,turn)
-        inc+=1
+            occupied=marking(input_from_user,turn)
 
         printboard(dict)
+        if occupied==1:
+            inc+=1
         chkwin=checkwin()
 
         if chkwin==1:
@@ -79,9 +90,3 @@ if __name__=='__main__':
 
         elif chkwin==-1:
             print("Match Draw!!")
-
-
-        
-
-
-
